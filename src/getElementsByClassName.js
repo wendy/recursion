3f03e3bc184e085ed653dfcbedb54971a9866762
Wendy Leung <wendysarahleung@gmail.com>
2015-01-12 21:26:28 -0800

    Updated StringifyJSON -no longer changes reference to obj

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,28 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   var body = document.body
   var array = [];
-  // your code here
-  var addElementsByClassName = function(element) {
-		var elementClassList = element.classList;
-	  if (elementClassList === undefined || elementClassList.length === 0) {
-		} else {
-			for(var i = 0; i < elementClassList.length; i++) {
-				if (elementClassList[i] === className) {
-					array.push(element);
-				}
-			}
-		};
 
-	  var elementChildNodes = element.childNodes
-	  if (elementChildNodes.length > 0) {
-	  	for(var i = 0; i < elementChildNodes.length; i++) {
-	  		addElementsByClassName(elementChildNodes[i]);
-	  	}
-	  }; 
+  var addElementsByClassName = function(element) {
+    var elementClassList = element.classList;
+    if (!(elementClassList === undefined || elementClassList.length === 0)) {
+      for(var i = 0; i < elementClassList.length; i++) {
+        if (elementClassList[i] === className) {
+            array.push(element);
+        }
+      }
+    };
+    var elementChildNodes = element.childNodes;
+    if (elementChildNodes.length > 0) {
+      for(var i = 0; i < elementChildNodes.length; i++) {
+          addElementsByClassName(elementChildNodes[i]);
+      }
+    }; 
   };
   
   addElementsByClassName(body);
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,94 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-  if (nullstringcheck(obj)) {
-    return nullstring(obj); 
+  if (obj === null){
+    return 'null';
+  } else if (typeof obj === "function" || obj === undefined) {
+    return undefined;
+  } else if (typeof obj === "string"){
+    return '"' + obj + '"';
   } else if (Array.isArray(obj)){
-  	return stringifyArray(obj);
-  } else if (typeof obj === 'object'){
-  	return stringifyObj(obj)
-  } else {
-  	return obj.toString();
-  }
-};
+    var array = [];
 
-var stringifyObj = function(obj) {
-  for (var key in obj) {
-  	if (nullstringcheck(obj[key])) {
-    	if (obj[key] === undefined) {
-    		delete obj[key];
-    	}else if (typeof obj[key] === "function") {
+    for (var i = 0; i < obj.length; i ++) {
+      array.push(stringifyJSON(obj[i]));
+    }
+
+    return '[' + array.join(',') + ']';
+  } else if (typeof obj === 'object'){
+    var array = [];
+
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === "function") {
         delete obj[key];
-    	} else {
-  	    obj[key] = nullstring(obj[key]);
-  	  }
-  	}
-  	if (typeof obj[key] === 'object' && obj[key] !== null) {
-      if (Array.isArray(obj[key])) {
-        obj[key] = stringifyArray(obj[key]);
       } else {
-        stringifyObj(obj[key]); 
-        obj[key] = hash(obj[key]);
+        array.push('"' + key + '":' + stringifyJSON(obj[key]));
       }
     }
-    
-  }
-obj = hash(obj);
-return obj;
-}
 
-var hash = function (obj) {
-  var array =[];
-  for (var key in obj) {
-    array.push('"' + key + '":' + obj[key]);
-  }
-  return '{' + array.join(',') + '}';
-}
-
-var stringifyArray = function(arr) {
-	for (var i = 0; i < arr.length; i ++) {
-		if (nullstringcheck(arr[i])) {
-			arr[i] = nullstring(arr[i]);
-		}
-	  if (Array.isArray(arr[i])) {
-	    stringifyArray(arr[i]);
-	    arr[i] = '[' + arr[i].join(',') + ']';
-		} else if (typeof arr[i] === 'object') {
-			arr[i] = stringifyObj(arr[i]);
-		}
-	}
-
-	return '[' + arr.join(',') + ']';
-  
-};
-
-var nullstringcheck = function(element) {
-  if (typeof element === "function") {
-    return true;
-  } else if (element === null){
-    return true;
-  } else if (typeof element === "string"){
-  	return true;
-  } else if (element === undefined) {
-    return true;
-  } else if (element[0] === '{' || element[0] === '[') {
-  	return false;
-  } else{
-  	return false;
-  }
-};
-
-var nullstring = function(element){
-  if (element === null){
-    return "null";
-  } else if (element === undefined) {
-    return "";
-  } else if (typeof element === "function") {
-    return "";
+    return '{' + array.join(',') + '}';
   } else {
-  	return '"' + element + '"';
+    return obj.toString();
   }
 };
+
 
 
